ccc8216768b5bfaf1bbb39101885c647bc27f31b
Mickael KERJEAN <mickael.kerjean@gmail.com>
2019-03-19 16:33:33 +1100

    test0 (draft): first draft

--- src/admin_console.test.js
+++ src/admin_console.test.js
@@ -1,6 +1,10 @@
 describe('Admin page', () => {
     beforeAll(async () => {
         await page.goto("http://demo.filestash.app/admin");
+        await expect(page).toMatchElement("input[placeholder=\"Password\"]", {timeout: 20000});
+        const password = "POUETTE";
+        expect(page).toFill("input[placeholder=\"Password\"]", password);
+        await page.keyboard.press('Enter');
     }, 20000);
 
     it("takes a screenshot of the dashboard", async () => {
